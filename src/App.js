daeeaed040fce9a47da7ed395ed68b60a85e1e46
Ellie <elishevaku@gmail.com>
2021-04-26 22:03:14 -0400

    subbreeds showing

--- src/App.js
+++ src/App.js
@@ -7,14 +7,24 @@ export default class FetchDog extends React.Component {
     doggies: [],
   }
 
+  //link API and define content
   async componentDidMount() {
     const url = 'https://dog.ceo/api/breeds/list/all'
     const response = await fetch(url)
     const data = await response.json()
     var result = Object.keys(data.message)
-    this.setState({ doggies: result, loading: false })
+    result.forEach((breed) => {
+        if (data.message[breed].length > 0) {
+          console.log("THE BREEDS")
+          const subBreeds = data.message[breed]
+          console.log(subBreeds)
+        }
+        
+    }); 
+    this.setState({ doggies: result, loading: false, data: data })
    }
 
+//check to see if its loading
   render() {
     if (this.state.loading) {
       return <div>loading...</div>
@@ -23,12 +33,17 @@ export default class FetchDog extends React.Component {
     if (!this.state.doggies) {
       return <div>didn't get a doggies</div>
     }
-
+//print out the stuff
     return (
       <div>
         {this.state.doggies.map((breed) => (
           <div>
             <div>{breed}</div>
+            {this.state.data.message[breed].map((subBreed) => (
+              <div> 
+                <div>{subBreed}</div>
+              </div>
+            ))}
           </div>
         ))}
       </div>
